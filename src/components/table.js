cfffe6b1ce7ae0ea6bd7c5164949136f1e8ddfe2
usagi-f <usagi.f@gmail.com>
2016-12-23 18:07:33 +0900

    update addItem function

--- src/components/table.js
+++ src/components/table.js
@@ -2,13 +2,17 @@ import Inferno from 'inferno';
 
 const Table = (props) => {
     const row = () => {
+        if (props.station.length === 0) return false;
+
         return props.station.map(elm => {
-            return <div>{elm.name}</div>
+            return <div>{elm}</div>
         });
     };
 
     return (
         <div>
+            {props.num}
+            <button onClick={props.handleClick}>click</button>
             {row()}
         </div>
     );
--- src/containers/viewContainer.js
+++ src/containers/viewContainer.js
@@ -7,11 +7,27 @@ import Table from './../components/table';
 import data from './../data/index';
 
 class ViewContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: 0,
+            viewStation: []
+        }
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem() {
+        if (data.station.length <= this.state.num) return false;
+        const item = data.station[this.state.num].name;
+        this.setState({
+            num: this.state.num + 1,
+            viewStation: this.state.viewStation.concat(item)
+        })
+    }
     render() {
         return (
             <div>
                 <Heading label="Inferno Data View" />
-                <Table station={data.station} />
+                <Table station={this.state.viewStation} handleClick={this.addItem} />
             </div>
         )
     }
